8e09bc5a75917ed612d4e5d499f37cf88cd4b279
ArmyaAli <ali.umar.work@gmail.com>
2021-03-01 20:16:07 +0000

    Deploying to pages from @ ArmyaAli/website@db3e774d1894d2cf092589b6d170be4bebcf101a 🚀

--- _next/static/_lNZ8tyzzMTnjdqhzp4s5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e0064994aea2770fc5bd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3d675fc3fd442ab22db0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

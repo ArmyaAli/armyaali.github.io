97ff966e7ab3f4569c662b63617b3ef24a9168df
ArmyaAli <ali.umar.work@gmail.com>
2021-02-28 20:58:04 -0500

    initial commit nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import { hostname } from "os";
+import styles from "../styles/Home.module.css";
+
+export const Home = () => {
+  return <div>Testing github pages...</div>;
+}
+
+export default Home;

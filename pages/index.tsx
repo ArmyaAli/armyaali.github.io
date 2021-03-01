17be1db48c6ade21b842c471c9e38ff986edc3c1
ArmyaAli <ali.umar.work@gmail.com>
2021-03-01 16:21:04 -0500

    added ts-particles to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import React from "react";
 import { Navbar } from "../components/navbar";
+import Particles from "react-tsparticles";
+import { config } from "../util/ts-particles-config";
+
 import styles from "../styles/Home.module.css";
 
 export const Home = () => {
@@ -9,6 +12,7 @@ export const Home = () => {
       <Head>
         <title>Personal Website</title>
       </Head>
+      <Particles id="tsparticles" options={config} />
       <Navbar />
     </div>
   );

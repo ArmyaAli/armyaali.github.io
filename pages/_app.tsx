91f21fcef96ca8b9a70a64715d53e65126c1dec8
ArmyaAli <ali.umar.work@gmail.com>
2021-03-02 14:06:04 -0500

    added tailwind import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default App;

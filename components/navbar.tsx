049a6df95344776cbf1d5d60747e5ebbd64c804e
ArmyaAli <ali.umar.work@gmail.com>
2021-03-02 14:06:12 -0500

    refactor

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import styles from "../styles/Navbar.module.css";
+import "tailwindcss/tailwind.css";
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav className={"navbar navbar-dark bg-dark"}>
-        <span className={styles.white + " " + ".navbar-brand"}>
-          Ali's Website
-        </span>
-      </nav>
-    </div>
+    <nav>
+    </nav>
   );
 };

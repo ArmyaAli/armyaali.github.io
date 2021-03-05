49c951e700efba8172f3c79f27af8e0b4464b8dc
ArmyaAli <ali.umar.work@gmail.com>
2021-03-05 15:58:19 -0500

    changing navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="p-3 flex justify-between items-center text-white">
-       <span>Welcome</span>
-       <ul className="flex text-xs justify-between w-96">
-           <li>About Me</li>
-           <li>Resume</li>
-           <li>Projects</li>
-           <li>About Me</li>
-           <li>Contact</li>
-       </ul>
+    <nav className="p-3 w-4/5 mx-auto my-8 flex justify-between items-center text-white bg-opacity-50">
+      <ul className="flex justify-between w-full h-full cursor-pointer text-center">
+        <li className="w-full hover:bg-gray-400 my-auto">Home</li>
+        <li className="w-full hover:bg-gray-400 my-auto">Projects</li>
+        <li className="w-full hover:bg-gray-400 my-auto">Resume</li>
+        <li className="w-full hover:bg-gray-400 my-auto">Contact</li>
+      </ul>
     </nav>
   );
 };

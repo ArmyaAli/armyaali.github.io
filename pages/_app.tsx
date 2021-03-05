d8f94ff35534b16ccb7f7047a6b832fc4db7f83c
ArmyaAli <ali.umar.work@gmail.com>
2021-03-04 20:42:10 -0500

    added particles component at top level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { AppProps } from "next/app";
+import React from "react";
+import Particles from "react-tsparticles";
+import { config } from "../util/ts-particles-config";
+
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Particles id="tsparticles" className="particles" options={config} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;

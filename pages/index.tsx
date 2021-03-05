e15939f2e4c38c404902b098d753964fadb04e59
ArmyaAli <ali.umar.work@gmail.com>
2021-03-05 16:23:04 -0500

    removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import { Navbar } from "../components/navbar";
-import Particles from "react-tsparticles";
-import { config } from "../util/ts-particles-config";
 import LandingPageContent from "../components/LandingPageContent";
 
 export const Home = () => {

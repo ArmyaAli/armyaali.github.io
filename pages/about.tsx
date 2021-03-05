ff36da3b3b46ad560d894ad77faeec3f6d2bd0f7
ArmyaAli <ali.umar.work@gmail.com>
2021-03-05 15:57:36 -0500

    added navbar to about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Card from "../components/card";
-import { fivews, shorts } from "./about_data";
+import { Navbar } from "../components/navbar";
+import { fivews, shorts } from "../util/about_data";
 export const About = () => {
   return (
-    <div className="flex flex-wrap">
-      {fivews.map((w) => (
-        <Card
-          title={w}
-          shortlist={shorts[w as "Who" | "What" | "Where" | "When" | "Why"]}
-        />
-      ))}
+    <div>
+        <Navbar />
+      <div className="flex flex-wrap">
+        {fivews.map((w) => (
+          <Card
+            title={w}
+            shortlist={shorts[w as "Who" | "What" | "Where" | "When" | "Why"]}
+          />
+        ))}
+      </div>
     </div>
   );
 };

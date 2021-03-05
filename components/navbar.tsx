46359dea2d6ccc750e513d620c13f4eeb5f35b16
ArmyaAli <ali.umar.work@gmail.com>
2021-03-05 16:23:14 -0500

    updated nav with routing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
   return (
     <nav className="p-3 w-4/5 mx-auto my-8 flex justify-between items-center text-white bg-opacity-50">
-      <ul className="flex justify-between w-full h-full cursor-pointer text-center">
-        <li className="w-full hover:bg-gray-400 my-auto">Home</li>
-        <li className="w-full hover:bg-gray-400 my-auto">Projects</li>
-        <li className="w-full hover:bg-gray-400 my-auto">Resume</li>
-        <li className="w-full hover:bg-gray-400 my-auto">Contact</li>
+      <ul className="flex justify-between w-full h-full cursor-pointer text-sm md:text-md lg:text-lg xl:text-3xl xl:h-16 3xl:text-4xl text-center border border-gray-500 rounded">
+        <Link href="/">
+          <li className="w-full hover:bg-gray-400 my-auto rounded">Home</li>
+        </Link>
+        <Link href="/projects">
+          <li className="w-full hover:bg-gray-400 my-auto rounded">Projects</li>
+        </Link>
+        <Link href="/resume">
+          <li className="w-full hover:bg-gray-400 my-auto rounded">Resume</li>
+        </Link>
+        <Link href="/contact">
+          <li className="w-full hover:bg-gray-400 my-auto rounded">Contact</li>
+        </Link>
       </ul>
     </nav>
   );

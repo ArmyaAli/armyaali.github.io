63441b56064b44301fec7d09d5bac8e71602b043
ArmyaAli <ali.umar.work@gmail.com>
2021-03-04 21:21:31 -0500

    improvement fixes for landing page

--- components/LandingPageContent.tsx
+++ components/LandingPageContent.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 
 export const LandingPageContent = () => {
   return (
-    <div className="text-white flex h-screen flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-col 2xl:flex-row">
-      <div className="m-auto relative">
+    <div className="p-4 text-white flex h-screen flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-col 2xl:flex-row">
+      <div className="m-auto xl:w-3/6 2xl:w-3/6 3xl:w-3/6 relative">
         <div className="text-5xl md:text-7xl xl:text-6xl 2xl:text-8xl 3xl:text-9xl">
           Ali Umar
         </div>
         <div className="text-5xl md:text-7xl xl:text-6xl 2xl:text-8xl 3xl:text-9xl">
           Jr Developer
         </div>
+        <p className="w-full my-8 breakwords">
+          Hi, my name is Ali. I'm 23 and I live in Toronto, Ontario. I love
+          programming and learning different kinds of technologies. Currently a
+          student at Seneca College studying Computer Engineering Technology. I
+          am not much of an artist but I tried to make this website look cool.
+          It's built using Nextjs, TailwindCSS and hosted on Github Pages!
+          
+          Interests: Video Game programming, Rust programming language,
+          Distributed Systems, Linux, Operating Systems, Compiler Design...
+        </p>
+
         <Link href="/aboutme">
           <button className="bg-red-800 w-full xl:w-96 h-16 opacity-50 rounded hover:opacity-100 mb-8 mt-8">
             Explore
@@ -18,7 +29,7 @@ export const LandingPageContent = () => {
         </Link>
       </div>
       <a
-        className="demo m-auto"
+        className="demo m-auto xl:w-2/5"
         href="https://github.com/ArmyaAli/Mushroom_Fighter"
       >
         <img

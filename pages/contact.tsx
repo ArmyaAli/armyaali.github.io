d102d24e8943733d3c371a25c1fadd5bc86f13d4
ArmyaAli <ali.umar.work@gmail.com>
2021-03-09 22:28:18 -0500

    moved to src folder

--- pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import { hostname } from "os";
-import styles from "../styles/Home.module.css";
-
-export const Contact = () => {
-  return <div>This is my contact page lmao</div>;
-}
-
-export default Contact;
--- pages/resume.tsx
+++
@@ -1,8 +0,0 @@
-import Head from "next/head";
-import { hostname } from "os";
-
-export const Resume = () => {
-  return <div>This is my Resume page lmao</div>;
-}
-
-export default Resume;

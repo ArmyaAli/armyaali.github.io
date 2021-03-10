63f4c46cd5ea4397167e51c83ab4d43bb2901127
ArmyaAli <ali.umar.work@gmail.com>
2021-03-10 14:12:16 -0500

    removed adsense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ export const Home = () => {
     <div>
       <Head>
         <title>Personal Website</title>
-        <script data-ad-client="ca-pub-4302156368235527" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <LandingPageContent />
     </div>

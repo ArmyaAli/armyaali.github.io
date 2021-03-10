f24e9437cb71349590397676cffdd217b8b83d5d
ArmyaAli <ali.umar.work@gmail.com>
2021-03-10 13:28:51 -0500

    will look at this later...

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,26 +1,11 @@
-import Head from "next/head";
-import React, { useState } from "react";
-import { Document, Page } from 'react-pdf/dist/umd/entry.parcel';
-
+import react from 'react'
 
 export const Resume = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const onDocumentLoadSuccess = ({ numPages }: any) => {
-    setNumPages(numPages);
-  }
-
-  return (
-    <div>
-      <Document file="resume_redacted.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
-  );
+    return (
+      <div>
+          No resume boss
+      </div>
+    );
 };
 
 export default Resume;

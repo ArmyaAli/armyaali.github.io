e3c63cc66513763141dabed1ef84da8095f25404
ArmyaAli <ali.umar.work@gmail.com>
2021-03-10 23:42:45 -0500

    dummy page...

---
+++ src/pages/dummy.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Dummy = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto text-3xl text-white p-8">
+        I will add more content soon ! Don't have much time right now... Please
+        enjoy the bouncing particles !
+      </div>
+    </div>
+  );
+};
+
+export default Dummy;

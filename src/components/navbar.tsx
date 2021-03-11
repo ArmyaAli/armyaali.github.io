33c7eaafdb1325e7d6be5a0bc61deaf2ca5c04b9
ArmyaAli <ali.umar.work@gmail.com>
2021-03-11 15:41:24 -0500

    changed nav styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,18 +4,26 @@ import React from "react";
 export const Navbar = () => {
   return (
     <nav className="p-3 w-4/5 mx-auto my-8 flex justify-between items-center text-white bg-opacity-50">
-      <ul className="flex justify-between w-full h-full cursor-pointer text-sm md:text-md lg:text-lg xl:text-3xl xl:h-16 3xl:text-4xl text-center border border-gray-500 rounded">
+      <ul className="flex justify-between w-full h-full cursor-pointer text-sm md:text-md lg:text-lg xl:text-3xl xl:h-16 3xl:text-4xl text-center rounded">
         <Link href="/">
-          <li className="w-full hover:bg-gray-400 my-auto rounded">Home</li>
+          <li className="w-full h-full flex justify-center items-center bg-red-800 hover:bg-red-800 hover:bg-opacity-75 bg-opacity-50 m-auto mx-2 rounded">
+            Home
+          </li>
         </Link>
         <Link href="/projects">
-          <li className="w-full hover:bg-gray-400 my-auto rounded">Projects</li>
+        <li className="w-full h-full flex justify-center items-center bg-red-800 hover:bg-red-800 hover:bg-opacity-75 bg-opacity-50 m-auto mx-2 rounded">
+            <span>Projects</span>
+          </li>
         </Link>
         <Link href="/resume">
-          <li className="w-full hover:bg-gray-400 my-auto rounded">Resume</li>
+        <li className="w-full h-full flex justify-center items-center bg-red-800 hover:bg-red-800 hover:bg-opacity-75 bg-opacity-50 m-auto mx-2 rounded">
+            <span>Resume</span>
+          </li>
         </Link>
         <Link href="/contact">
-          <li className="w-full hover:bg-gray-400 my-auto rounded">Contact</li>
+        <li className="w-full h-full flex justify-center items-center bg-red-800 hover:bg-red-800 hover:bg-opacity-75 bg-opacity-50 m-auto mx-2 rounded">
+            <span>Contact</span>
+          </li>
         </Link>
       </ul>
     </nav>

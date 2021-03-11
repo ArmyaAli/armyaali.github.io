9e4b4e5a3c97efdf9fb2b9977fc79af9db9cd433
ArmyaAli <ali.umar.work@gmail.com>
2021-03-11 15:40:36 -0500

    updated

--- src/util/about_data.ts
+++ src/util/about_data.ts
@@ -1,9 +1,9 @@
-export const fivews = ["Who", "What", "Where", "When", "Why"];
+export const fivews = ["Who", "What", "Where", "When", "Why"]
 
 export const shorts = {
     "Who": [
         "Ali Umar",
-        "22 years old",
+        "23 years old",
         "Student",
         "Jr Developer",
         "Software Developer",
@@ -18,9 +18,10 @@ export const shorts = {
     ],
     "Where": [
         "Toronto, ON, Canada",
+        "Seneca College"
     ],
     "When": [
-        "present",
+        "I'm Alive !",
     ],
     "Why": [
         "Loves coding and has fun doing it",
@@ -28,6 +29,59 @@ export const shorts = {
         "Loves problem solving",
         "Need money to buy food",
     ],
+}
+
+export const longs = {
+    "Who": "",
+    "What": "",
+    "Where": "",
+    "When": "",
+    "Why": ""
+}
+
+export const categories = ["Foundational Knowledge", "Programming Languages", "Frameworks/Libraries", "Technologies"]
+
+export const skills = {
+    "Foundational Knowledge": [
+        "Generic Programming Concepts: (Control Flow, Data Types, Memory Management, Applications of Data Structures & Algorithms, GC vs non GC)",
+        "Programming language Paradigms: [OOP, Functional, Declaritive, Imperative]",
+        "Relational Databases",
+        "RegEx",
+        "Database Design",
+        "Problem Solving Skills: Top down, Bottom up, Greedy, Divide and Conqour",
+        "Using different APIs",
+        "Basic networking concepts",
+    ],
+    "Programming Languages": [
+        "C/C++",
+        "C# (One of my favourite)",
+        "Java",
+        "Python",
+        "JavaScript/TypeScript (This website was built using TypeScript :o)",
+    ],
+    "Frameworks/Libraries": [
+        "Microsoft Windows Forms",
+        ".NET",
+        "ASP.NET",
+        "Angular",
+        "React",
+        "NextJS",
+        "TailwindCSS",
+        "RxJS",
+        "DiscordJS",
+        "RaylibC",
+        "CheerioJS",
+        "PuppeteerJS",
+    ],
+    "Technologies": [
+        "Git Source Control",
+        "AWS EC2",
+        "Azure DevOps",
+        "Visual Studio/code",
+        "MSSQL Sever",
+        "Oracle SQL Developer",
+    ]
+
 }
 
 
